1930bf2aa04d0f8748c231efdec2855f2298a703
Kauê <kauetmurakami@gmail.com>
2024-08-16 18:05:16 -0300

    add styles and finish first app

--- src/App.vue
+++ src/App.vue
@@ -15,11 +15,27 @@ function decrement() {
 
 <template>
   
-    <h1>Count is: {{ counter.count }}</h1>
+    <div class="container">
+      <h1 class="centeredTitle">Count is: {{ counter.count }}</h1>
+    </div> 
     <br/>
-    <button type="button" class="btn btn-outline-primary btn-sm" v-on:click="incremment"> + </button>
+    <button type="button" class="btn btn-outline-primary btn-sm btn-mr" v-on:click="incremment"> + </button>
     <button type="button" class="btn btn-outline-danger btn-sm" v-on:click="decrement"> - </button>
 </template>
 
+<style>
+.btn-mr {
+  margin-right: 10px;
+}
+.container {
+  display: flex;
+  justify-content: center; /* Centraliza horizontalmente */
+  align-items: center; /* Centraliza verticalmente */
+  /* Define a altura do contêiner, pode ser ajustado conforme necessário */
+}
 
+.centeredTitle {
+  text-align: center; /* Opcional, centraliza o texto dentro do elemento */
+}
+</style>
 
