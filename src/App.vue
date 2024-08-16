08cc0a835d36905dae9e1c42bfba1572430ed4bd
Kauê <kauetmurakami@gmail.com>
2024-08-16 17:34:14 -0300

    initial update the first vue project

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { reactive, ref } from 'vue'
+
+const counter = reactive({ count: 0 })
+
+function incremment() {
+  counter.count++
+}
+
+function decrement() {
+  counter.count--
+}
+
+</script>
+
+<template>
+  
+  <h1>Count is: {{ counter.count }}</h1>
+  <br/>
+  <button v-on:click="incremment"> + </button>
+  <button v-on:click="decrement"> - </button>
+</template>
